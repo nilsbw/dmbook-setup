1743fc0ef2b7524dd5f1e5842843434c004dd890
Nils B. Weidmann <nils.weidmann@uni-konstanz.de>
2020-04-23 17:24:38 +0200

    Data preparation for Chapter 13.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# dmbook-data
-Data for the exercises in "Data Management for Social Scientists"
+# Data for the exercises in "Data Management for Social Scientists"
+
+This repository contains the necessary data files for the code and exercises presented in the book. 
+
+The data preparation script `prepare-data.R` downloads each dataset to a subfolder in the "raw" directory and applies the necessary modifications and copies the final files to one of the data subdirectories (`ch**`). 
+
+For readers of this book, there is no need to run this file, since the required data files can be obtained directly from this repository. 
+
+See the code in the data preparation script for the locations of the original files online, and the subfolders where they are stored locally.
+
+Many datasets are modified to facilitate presentation in the book. Modifications include the dropping of variables or cases, the renaming of files, or changes in the file format. See the code in the data preparation script for the modifications applied to the data.
--- prepare-data.R
+++ prepare-data.R
@@ -6,19 +6,6 @@ library(countrycode)
 # This file documents the generation of the data files for the exercises in 
 # Weidmann, Nils B. "Data Management for Social Scientists"
 
-# For readers of this book, there is no need to run this file, since the 
-# required data files can be obtained directly from the repository at
-# https://github.com/nilsbw/dmbook-exercises
-
-# This script downloads each dataset to a subfolder in the "raw" directory. 
-# See the code below for the locations of the original files online,
-# and the subfolders where they are stored locally.
-# Many datasets are modified to facilitate presentation in the book. 
-# Modifications include the dropping of variables or cases, the 
-# renaming of files, or changes in the file format. 
-# See the code below for the modifications applied to the data.
-
-
 # create directory for raw files
 dir.create(file.path("raw"), showWarnings = FALSE)
 
@@ -85,6 +72,7 @@ for (file in list.files(tmp, "^WID_data")) {
 }
 inequality <- read_delim(file.path("raw", "wid", "WID_data_US.csv"), delim=";") %>% filter(variable == "sptincj992" & percentile == "p90p100")
 write_delim(inequality, file.path("ch06", "us-inequality.csv"), delim = "," )
+unlink(tmp, recursive = TRUE)
 
 
 # US GDP estimates from the Federal Reserve Bank of St. Louis (https://fred.stlouisfed.org/series/A939RX0Q048SBEA)
@@ -163,31 +151,37 @@ read_delim(file.path("raw", "ged", "ged191.csv"), delim = ",") %>%
   filter(where_prec==1) %>%
   select(id, date_start, latitude, longitude, best) %>%
   write_csv(file.path("ch11", "ged.csv"))
+unlink(tmp, recursive = TRUE)
+
 
 ### Chapter 12
 
 ### Chapter 13
 
 ## COW Trade data
-download.file("https://correlatesofwar.org/data-sets/bilateral-trade/cow_trade_4.0/at_download/file", file.path("data", "ch13", "cow_trade_v4.zip"))
-unzip(file.path("data", "ch13", "cow_trade_v4.zip"), exdir=file.path("data", "ch13"))
-unlink(file.path("data", "ch13", "cow_trade_v4.zip"))
-read_delim(file.path("data", "ch13", "COW_Trade_4.0", "Dyadic_COW_4.0.csv"), delim = ",") %>%
-  filter(year >= 1870 & year <= 2014) %>%
+dir.create(file.path("raw", "cow-trade"), showWarnings = FALSE)
+tmp <- tempdir()
+download.file("https://correlatesofwar.org/data-sets/bilateral-trade/cow_trade_4.0/at_download/file", file.path(tmp, "cow_trade_v4.zip"))
+unzip(file.path(tmp, "cow_trade_v4.zip"), exdir=tmp)
+file.copy(file.path(tmp, "COW_Trade_4.0", "Dyadic_COW_4.0.csv"), file.path("raw", "cow-trade", "Dyadic_COW_4.0.csv"))
+file.copy(file.path(tmp, "COW_Trade_4.0", "National_COW_4.0.csv"), file.path("raw", "cow-trade", "National_COW_4.0.csv"))
+file.copy(file.path(tmp, "COW_Trade_4.0", "COW Trade Data Set Codebook.pdf"), file.path("raw", "cow-trade", "COW Trade Data Set Codebook.pdf"))
+read_delim(file.path("raw", "cow-trade", "Dyadic_COW_4.0.csv"), delim = ",") %>%
+  filter(year >= 1946 & year <= 2014) %>%
   select(ccode1, ccode2, year, smoothflow1, smoothflow2, smoothtotrade) %>%
-  write_csv(file.path("data", "ch13", "trade.csv"), na="NA")
-unlink(file.path("data", "ch13", "COW_Trade_4.0"), recursive = T)
-unlink(file.path("data", "ch13", "__MACOSX"), recursive=T)
+  write_csv(gzfile(file.path("ch13", "trade.csv.gz")), na="NA")
+unlink(tmp, recursive = TRUE)
 
 # V-Dem data
-download.file("http://v-dem.pol.gu.se/v9/Country_Year_V-Dem_Full+others_R_v9.zip", file.path("data", "ch13", "vdem-complete.zip"))
-unzip(file.path("data", "ch13", "vdem-complete.zip"), exdir=file.path("data", "ch13"))
-vdem <- readRDS(file.path("data", "ch13", "Country_Year_V-Dem_Full+others_R_v9", "V-Dem-CY-Full+Others-v9.rds"))
+dir.create(file.path("raw", "v-dem"), showWarnings = FALSE)
+tmp <- tempdir()
+download.file("http://v-dem.pol.gu.se/v9/Country_Year_V-Dem_Full+others_R_v9.zip", file.path(tmp, "vdem-complete.zip"))
+unzip(file.path(tmp, "vdem-complete.zip"), exdir=tmp)
+file.copy(file.path(tmp, "Country_Year_V-Dem_Full+others_R_v9", "V-Dem-CY-Full+Others-v9.rds"), file.path("raw", "v-dem", "V-Dem-CY-Full+Others-v9.rds"))
+vdem <- readRDS(file.path("raw", "v-dem", "V-Dem-CY-Full+Others-v9.rds"))
 vdem %>%
-  filter(year >= 1870 & year <= 2014) %>%
+  filter(year >= 1946 & year <= 2014) %>%
   select(country_name, year, COWcode, v2x_polyarchy, e_regiongeo, e_migdppc) %>%
-  write_csv(file.path("data", "ch13", "vdem.csv"), na="NA")
-unlink(file.path("data", "ch13", "Country_Year_V-Dem_Full+others_R_v9"), recursive=T)
-unlink(file.path("data", "ch13", "vdem-complete.zip"))
-unlink(file.path("data", "ch13", "__MACOSX"), recursive=T)
+  write_csv(file.path("ch13", "vdem.csv"), na="NA")
+unlink(tmp, recursive = TRUE)
 
